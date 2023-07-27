7e524916b33d7c918d873bb877183bf9eed7897e
Sumandari <sumandari.ee@gmail.com>
2023-07-27 09:09:34 +0200

    useReducer

--- src/PersonScore.tsx
+++ src/PersonScore.tsx
@@ -1,17 +1,52 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer, useState } from 'react';
 import { getPerson } from './getPerson';
 
+type State = {
+  name: string | undefined;
+  score: number;
+  loading: boolean;
+};
+
+type Action =
+  | {
+      type: 'initialize';
+      name: string;
+    }
+  | {
+      type: 'increment';
+    }
+  | {
+      type: 'decrement';
+    }
+  | {
+      type: 'reset';
+    };
+
+function reducer(state: State, action: Action): State {
+  switch (action.type) {
+    case 'initialize':
+      return { name: action.name, score: 0, loading: false };
+    case 'increment':
+      return { ...state, score: state.score + 1 };
+    case 'decrement':
+      return { ...state, score: state.score - 1 };
+    case 'reset':
+      return { ...state, score: 0 };
+    default:
+      return state;
+  }
+}
+
 export function PersonScore() {
-  const [name, setName] = useState<string | undefined>();
-  const [score, setScore] = useState(0);
-  const [loading, setLoading] = useState(true);
+  const [{ name, score, loading }, dispatch] = useReducer(reducer, {
+    name: undefined,
+    score: 0,
+    loading: true,
+  });
+
   useEffect(() => {
-    getPerson().then((person) => {
-      setLoading(false);
-      setName(person.name);
-    });
+    getPerson().then(({ name }) => dispatch({ type: 'initialize', name }));
   }, []);
-  console.log('state values', loading, name);
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -22,9 +57,9 @@ export function PersonScore() {
         {name}, {score}
       </h3>
 
-      <button onClick={() => setScore(score + 1)}>Add</button>
-      <button onClick={() => setScore(score - 1)}>Substract</button>
-      <button onClick={() => setScore(0)}>Reset</button>
+      <button onClick={() => dispatch({ type: 'increment' })}>Add</button>
+      <button onClick={() => dispatch({ type: 'decrement' })}>Substract</button>
+      <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
     </div>
   );
 }
